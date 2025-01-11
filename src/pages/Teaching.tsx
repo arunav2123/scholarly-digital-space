85f5773aaafd84cf288a165ea14535be202790fa
Arunav <44284909+arunav2123@users.noreply.github.com>
2025-01-11 02:22:28 -0500

    Update Teaching.tsx

--- src/pages/Teaching.tsx
+++ src/pages/Teaching.tsx
@@ -13,13 +13,16 @@ const Teaching = () => {
               <div className="card">
                 <h3 className="text-xl font-semibold mb-2">Mathematical Sciences Institute, ANU</h3>
                 <ul className="list-disc list-inside mt-4 text-academic-secondary">
-                  <li><strong>Tutor</strong> - Semester I 2023: MATH1014 & MATH2305 (till 20.04.2023)</li>
-                  <li><strong>Co-lecturer</strong> - MATH1014 Spring Course 2022 - Mathematics with Applications II: sequences and series, multivariable calculus, numerical optimization</li>
-                  <li><strong>Tutor and Workshop Demonstrator</strong>:
+                  <li><strong>Co-lecturer</strong> - MATH1014 Spring Course 2022 - Mathematics with Applications II  
+                         <\br>
+                         Topics Include - Sequences and series, multivariable calculus, numerical optimization</li>
+                  <li><strong>Academic tutor and workshop demonstrator</strong>:
                     <ul className="list-decimal list-inside ml-4">
+                      <li>MATH2305 - Semester I 2023 - Applied Mathematics I (till 20.04.2023)</li> 
+                      <li>MATH1014 - Semester I 2023 - Applied Mathematics II (till 20.04.2023)</li>
                       <li>MATH2306 - Semester II 2022 - Applied Mathematics II</li>
                       <li>MATH1115 - Semester I 2022 - Advanced Mathematics and Applications I</li>
-                      <li>MATH2305 - Semester I 2022 - Applied Mathematics I</li>
+                      <li>MATH2305 - Semester I 2022 - Applied Mathematics I </li>
                       <li>Bridging Course Module 3 - Advanced MATH1115, Feb 2022</li>
                       <li>Advanced PDEs Course (Shandong University, China in collaboration with MSI)</li>
                       <li>MATH2306 - Semester II 2021 - Applied Mathematics II</li>
